af0d12f6f96eff62e12addaf3391fbbdd12af09c
AnejVovcak <anej.vovcak@gmail.com>
2022-05-15 04:52:54 +0200

    zelo dobro! zmagoslavni sprehod na obzorju

--- vid.py
+++ vid.py
@@ -4,7 +4,6 @@ import imutils
 import numpy as np
 
 data = {}
-
 backgroundIteration = 10
 
 
@@ -24,15 +23,44 @@ def getColor(colorIndex):
 
     return colors[colorIndex]
 
-def RGB2HEX(color):
-    return '#'+hex((color[0] << 16) + (color[1] << 8) + (color[2])).upper()[2:]
+def RGB2HEX(colorIndex):
+    colors = {
+        0 : "#FFFF00",
+        1 : "#00FF00",
+        2 : "#FF0000",
+        3 : "#0000FF",
+        4 : "#000000"
+    }
+    return colors[colorIndex]
+
+def Vrsta(colorIndex):
+    
+    if colorIndex<2:
+        return 2
+    elif colorIndex<4:
+        return 1
+    return 0
+
+
+def grabCenters(frame,colorRange):
+    
+    frame = cv2.GaussianBlur(frame,(9,9),0)  #Izniči šum
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV) # Pretvori v HSV vrednosti
+    frame = cv2.erode(frame, None) #Pomanjšaj in povečaj da še bolj izničiš šum
+    frame = cv2.dilate(frame, None) 
+    frame = cv2.inRange(frame,colorRange[0],colorRange[1]) #Ustvari masko glede na vrednosti posameznih pikslov
+    centers = cv2.findContours(frame.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) #najdi posamezne dele ki jih maska ne pokriva
+    cnts = imutils.grab_contours(centers) #pretvori posamezne dele v array
+    return cnts
+
+
 
 def colorDetection(frame):
     global data
-    data = {}
 
+    newdata = {}
 
-
+    #Array mejnih vrednosti posameznih barv
     colorRanges = [
         [(16,68,112),(33,227,247)], #yellow
         [(40,68,112),(56,227,247)], #green
@@ -42,116 +70,61 @@ def colorDetection(frame):
         ]
     
     original = frame.copy()
-    obj = 0
-    for colorIndex,colorRange in enumerate(colorRanges):
+    objId = 0
 
-        frame = original.copy()
-        frame = cv2.GaussianBlur(frame,(9,9),0)
-        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-        frame = cv2.erode(frame, None)
-        frame = cv2.dilate(frame, None)
-        frame = cv2.inRange(frame,colorRange[0],colorRange[1])
-        centers = cv2.findContours(frame.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(centers)
+    for colorIndex,colorRange in enumerate(colorRanges):   #Za vsako barvo
 
+        cnts = grabCenters(original.copy(),colorRange) #Poišči centre najdenih delov
 
-        if len(cnts) > 0:           
-            for c in cnts:
-                ((x, y), radius) = cv2.minEnclosingCircle(c)
-                x = int(x)
-                y = int(y)
-                color = getColor(colorIndex)
+                   
+        for c in cnts:
+            ((x, y), radius) = cv2.minEnclosingCircle(c) #najmanjši očrtani krog okoli najdenih delov
+            x = int(x)
+            y = int(y)
+
+            color = getColor(colorIndex)  #Barva balinčka
                 
-                if radius > 15:
-                    cv2.circle(original, (int(x), int(y)), int(radius),(int(color[0]),int(color[1]),int(color[2])), 2)
-                    cv2.putText(original, str(obj)+' : ' + str(Vrsta(color,radius)) + ' : ' + str(color), (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-                    data[str(obj)] = {
-                        "x" : int(x),
-                        "y" : int(y),
-                        "barva" :  RGB2HEX(color[::-1]),
-                        "type" : Vrsta(color,radius)
-                    }
-                    obj+=1
-                    #cv2.circle(frame, center, 5, (0, 0, 255), -1)
-                    #print(data)
+            if radius > 15:
+                cv2.circle(original, (int(x), int(y)), int(radius),(int(color[0]),int(color[1]),int(color[2])), 2)
+                cv2.putText(original, str(colorIndex)+' : ' + str(Vrsta(colorIndex)), (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+                
+                newdata[str(objId)] = {
+                    "x" : x,  #X koordinata
+                    "y" : y, #Y koordinata
+                    "barva" :  RGB2HEX(colorIndex), #Barva balinčka                #Zapiši podatke v dictionary za na server
+                    "type" : Vrsta(colorIndex) #Ekipa balinčka
+                }
+                
+                objId+=1
+
+    data = newdata
     return original
 
 
-def Vrsta(color,radius):
-    
-    blueUpper = (162, 176, 12)
-    blueLower= (84, 0, 27)
-   
-    if radius < 30:
-        return 0
-    else:
-        return 1
 
-
-def Mask(frame,ozadje):
-    original = frame.copy()
-    
-    # Remove background algorithm
-    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    frame = abs(ozadje-frame)
-    #frame = cv2.cvtColor(frame,cv2.COLOR_GRAY2BGR)
-
-    
-    frame = cv2.GaussianBlur(frame,(19,19),0)
-    ret3,frame = cv2.threshold(frame,100,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
-    cv2.imshow("frame",frame)
-    """
-    frame = cv2.cvtColor(frame,cv2.COLOR_GRAY2BGR)
-    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    frame = cv2.inRange(frame,(0,87,94),(179,222,150))
-    cv2.imshow("frame2",frame)
-"""
-    #contours,_ = cv2.findContours(frame.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    #cv2.drawContours(frame, contours, -1, (255),-1)
-    frame = cv2.bitwise_and(original,original,mask=frame)
-    return frame
-
-
-ozadje = None
 def zaznavanje():
 
-   
     cameraFeed = cv2.VideoCapture(0)
-    #cameraFeed = cv2.VideoCapture("output.avi")
-    time.sleep(1)
-    global ozadje
-    
-    for _ in range(backgroundIteration):
-        ret,frame = cameraFeed.read()
-        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        if ozadje is None:
-            ozadje = frame
-        else:
-            ozadje = cv2.addWeighted(frame,0.5,ozadje,0.5,0)
 
+    time.sleep(.2) #Čakaj da se kamera gotovo poveže
     
     while True:
 
 
-        ret,frame = cameraFeed.read()
+        ret,frame = cameraFeed.read()  #Beri frame iz streama in preveri uspešnost branja
         if frame is None:
             break
-
-
         
-        # cv2.imshow("Raw",frame);
-        # masked = Mask(frame,ozadje)
-        # cv2.imshow("Masked",masked);
-        frame = colorDetection(frame)
-        #cv2.imshow("Konec",frame)
+        frame = colorDetection(frame) #Zaznaj objekte
 
+        #cv2.imshow("Frame",frame) #Prikaži zaznane objekte na sliki (Zakomentirano za rPi)
+        #print(izpis())
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        #print(Izpis())
     
-    cameraFeed.release()
-    cv2.destroyAllWindows()
+    cameraFeed.release() #Sprosti procese in zapri odprta okna
+    cv2.destroyAllWindows() 
 
 #if __name__ == "__main__":
 #    zaznavanje()
